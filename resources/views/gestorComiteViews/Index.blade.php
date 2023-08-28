8301795d3ea2c00808c06fa9b1b1b3e56f25eb78
SQT11 <ensantyquintero@gmail.com>
2023-08-28 11:41:19 -0500

    modificando iconos de la vista gestor index

--- resources/views/gestorComiteViews/Index.blade.php
+++ resources/views/gestorComiteViews/Index.blade.php
@@ -7,21 +7,30 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
-                    <div class="relative">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                            </svg>
-                        </div>
-                        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
-                        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+            <div class="pb-8">
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white ">Buscar</label>
+                <div class="relative">
+
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
                     </div>
+                    <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar citaciones" required>
+                    <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @can('administrar')
+                        <x-link href="{{ route('gestorComiteViews.create') }}" class="m-4">Crear citación</x-link>
+                @endcan
+
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left text-gray-500 ">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
                             <tr>
+
                                 <th scope="col" class="px-6 py-3">
                                     aprendiz
                                 </th>
@@ -46,6 +55,7 @@
                         <tbody>
                             @forelse ($solicituds as $solicitud)
                                 <tr class="bg-white border-b ">
+
                                     <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                                         {{ $solicitud->aprendiz->apr_nombre }}
                                     </td>
@@ -53,7 +63,7 @@
                                     <td class="px-6 py-4 font-medium text-gray-900 truncate max-w-xs">
                                         {{ $solicitud->programa->pro_nombre }}
                                     </td>
-                                    
+
                                     {{-- <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                                         {{ $solicitud->}}
                                     </td> --}}
@@ -65,8 +75,10 @@
                                         {{ $solicitud->sol_estado}}
                                     </td>
                                 </tr>
+
                             @empty
                                 <tr class="bg-white border-b">
+
                                     <td colspan="5" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                         {{ __('No se encontraron solicitudes') }}
                                     </td>
--- resources/views/solicitudComites/index.blade.php
+++ resources/views/solicitudComites/index.blade.php
@@ -8,6 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @can('administrar')
+                        <x-link href="{{ route('gestorComiteViews.create') }}" class="m-4">Crear comite</x-link>
+                    @endcan
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left text-gray-500 ">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
@@ -47,16 +50,33 @@
                                         {{ $solicitudComite->sol_estado }}
                                     </td>
                                     @can('administrar')
-                                        <td class="px-6 py-4">
-                                            <form method="POST"
-                                                action="{{ route('solicitudComites.destroy', $solicitudComite) }}"
-                                                class="inline-block">
-                                                @csrf
-                                                @method('DELETE')
-                                                <x-danger-button type="submit" onclick="return confirm('¿Está seguro?')">
-                                                    Eliminar</x-danger-button>
-                                            </form>
+                                        <td>
+                                            <button>
+                                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.418 17.861 1 20l2.139-6.418m4.279 4.279 10.7-10.7a3.027 3.027 0 0 0-2.14-5.165c-.802 0-1.571.319-2.139.886l-10.7 10.7m4.279 4.279-4.279-4.279m2.139 2.14 7.844-7.844m-1.426-2.853 4.279 4.279"/>
+                                                </svg>
+                                            </button>
+
                                         </td>
+                                        <td>
+                                            <button>
+                                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.656 12.115a3 3 0 0 1 5.682-.015M13 5h3m-3 3h3m-3 3h3M2 1h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm6.5 4.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                                                </svg>
+                                            </button>
+
+                                        </td>
+
+
+                                        <td class="text-center">
+                                            <button>
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-9" viewBox="0 0 20 20" fill="blackColor">
+                                                    <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                                </svg>
+                                            </button>
+                                        </td>
+
+
                                     @endcan
                                 </tr>
                             @empty
