6a4bc7ba4369409cfa2d91be00538062be4b9743
Skeepx <maferestrepo90@gmail.com>
2023-09-19 12:28:30 -0500

    .-.

--- resources/views/instructorViews/registrar_novedades.blade.php
+++ resources/views/instructorViews/registrar_novedades.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="max-w-3xl mx-auto p-6 mt-10">
+    <div class="max-w-6xl mx-auto p-6 mt-10">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Registrar novedad') }}
