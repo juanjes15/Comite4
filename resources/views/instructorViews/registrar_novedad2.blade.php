64192f5f608d0f407d4eb7534fefa68b1b09b00b
Skeepx <maferestrepo90@gmail.com>
2023-10-19 08:23:58 -0500

    .

--- resources/views/instructorViews/registrar_novedad2.blade.php
+++ resources/views/instructorViews/registrar_novedad2.blade.php
@@ -17,7 +17,7 @@
                             <x-label for="ins_id" value="{{ __('Instructor') }}" />
                             <select name="ins_id"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                                <option value="">--Seleccione el Instructor--</option>
+                              
                                 @foreach ($instructors as $instructor)
                                     <option value="{{ $instructor->id }}">{{ $instructor->ins_nombres }}
                                         {{ $instructor->ins_apellidos }} - {{ $instructor->ins_area }} </option>
