029f2886b04d6f977038218d50ea698c44a23cc4
SQT11 <ensantyquintero@gmail.com>
2023-10-27 09:30:59 -0500

    referencia para cambiar el color del login

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,3 +1,5 @@
+
+
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         {{ $logo }}
@@ -7,3 +9,5 @@
         {{ $slot }}
     </div>
 </div>
+
+
