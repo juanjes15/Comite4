765527c0ffbc22778018f92b91157ad72035af6d
JJ <juanjes15@gmail.com>
2023-11-09 09:06:09 -0500

    admin rol

--- routes/web.php
+++ routes/web.php
@@ -25,34 +25,42 @@ Route::middleware([
     })->name('dashboard');
     //Route::get('/users', [UserController::class, 'index'])->name('users.index');
     //Route::view('/register', 'auth/register')->name('register');
-    Route::resources([
-        'programas' => \App\Http\Controllers\ProgramaController::class,
-        'fichas' => \App\Http\Controllers\FichaController::class,
-        'instructors' => \App\Http\Controllers\InstructorController::class,
-        'aprendizs' => \App\Http\Controllers\AprendizController::class,
-        'solicitudComites' => \App\Http\Controllers\SolicitudComiteController::class,
-        'comites' => \App\Http\Controllers\ComiteController::class,
-        'pruebas' => \App\Http\Controllers\PruebaController::class,
-        'capitulos' => \App\Http\Controllers\CapituloController::class,
-        'articulos' => \App\Http\Controllers\ArticuloController::class,
-        'numerals' => \App\Http\Controllers\NumeralController::class,
-        'gestorComiteViews' => \App\Http\Controllers\GestorComiteViewsController::class,
-    ]);
-    Route::get('/users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [\App\Http\Controllers\UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [\App\Http\Controllers\UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{user}/edit', [\App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [\App\Http\Controllers\UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{user}', [\App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
 
-    Route::get('/users/addEstudiante/', [\App\Http\Controllers\UserController::class, 'addEstudiante'])->name('users.addEstudiante');
-    Route::put('/users/storeEstudiante/{user}', [\App\Http\Controllers\UserController::class, 'storeEstudiante'])->name('users.storeEstudiante');
+    //Rutas para el administrador
+    Route::middleware('checkUserRole:Administrador')->group(function () {
+        Route::resources([
+            'programas' => \App\Http\Controllers\ProgramaController::class,
+            'fichas' => \App\Http\Controllers\FichaController::class,
+            'instructors' => \App\Http\Controllers\InstructorController::class,
+            'aprendizs' => \App\Http\Controllers\AprendizController::class,
+            'solicitudComites' => \App\Http\Controllers\SolicitudComiteController::class,
+            'comites' => \App\Http\Controllers\ComiteController::class,
+            'pruebas' => \App\Http\Controllers\PruebaController::class,
+            'capitulos' => \App\Http\Controllers\CapituloController::class,
+            'articulos' => \App\Http\Controllers\ArticuloController::class,
+            'numerals' => \App\Http\Controllers\NumeralController::class,
+            'gestorComiteViews' => \App\Http\Controllers\GestorComiteViewsController::class,
+        ]);
+        Route::get('/users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+        Route::get('/users/create', [\App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+        Route::post('/users', [\App\Http\Controllers\UserController::class, 'store'])->name('users.store');
+        Route::get('/users/{user}/edit', [\App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+        Route::put('/users/{user}', [\App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+        Route::delete('/users/{user}', [\App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
 
-    Route::get('/users/addInstructor/', [\App\Http\Controllers\UserController::class, 'addInstructor'])->name('users.addInstructor');
-    Route::put('/users/storeInstructor/{user}', [\App\Http\Controllers\UserController::class, 'storeInstructor'])->name('users.storeInstructor');
+        Route::get('/users/addEstudiante/', [\App\Http\Controllers\UserController::class, 'addEstudiante'])->name('users.addEstudiante');
+        Route::put('/users/storeEstudiante/{user}', [\App\Http\Controllers\UserController::class, 'storeEstudiante'])->name('users.storeEstudiante');
+
+        Route::get('/users/addInstructor/', [\App\Http\Controllers\UserController::class, 'addInstructor'])->name('users.addInstructor');
+        Route::put('/users/storeInstructor/{user}', [\App\Http\Controllers\UserController::class, 'storeInstructor'])->name('users.storeInstructor');
+    });
+
+    //Rutas para el instructor
+    Route::middleware('checkUserRole:Instructor')->group(function () {
+    });
 
     Route::post('/subir', 'Controller@subirArchivo')->name('subir');
-    
+
     //Instructor
     Route::get('/InstructorViews/solicitar1', [\App\Http\Controllers\InstructorViewController::class, 'solicitar1'])->name('instructorViews.solicitar1');
     Route::get('/InstructorViews/solicitar2', [\App\Http\Controllers\InstructorViewController::class, 'solicitar2'])->name('instructorViews.solicitar2');
@@ -79,12 +87,12 @@ Route::middleware([
 
     Route::get('/InstructorViews/registrar_novedad5/{sol_id}', [\App\Http\Controllers\InstructorViewController::class, 'registrar_novedad5'])->name('instructorViews.registrar_novedad5');
     Route::put('/InstructorViews/registrar_novedad5/{sol_id}', [\App\Http\Controllers\InstructorViewController::class, 'storeRegistrar_novedad5'])->name('instructorViews.storeRegistrar_novedad5');
-    
+
     Route::match(['get', 'put'], '/instructorViews/detalles_comite/{solicitud}', [\App\Http\Controllers\InstructorViewController::class, 'detalles_comite'])
-    ->name('instructorViews.detalles_comite');
+        ->name('instructorViews.detalles_comite');
 
     //------------------
-    
+
     Route::get('/InstructorViews/solicitarResumen', [\App\Http\Controllers\InstructorViewController::class, 'solicitarResumen'])->name('instructorViews.solicitarResumen');
     Route::get('/instructorViews/plan_MejoramientoP', [\App\Http\Controllers\InstructorViewController::class, 'plan_MejoramientoP'])->name('instructorViews.plan_MejoramientoP');
     Route::get('/instructorViews/plan_Mejoramiento', [\App\Http\Controllers\InstructorViewController::class, 'plan_Mejoramiento'])->name('instructorViews.plan_Mejoramiento');
@@ -93,16 +101,16 @@ Route::middleware([
     Route::get('/instructorViews/detalles_antecedentes', [\App\Http\Controllers\InstructorViewController::class, 'detalles_antecedentes'])->name('instructorViews.detalles_antecedentes');
     Route::get('/instructorViews/consultar_comite', [\App\Http\Controllers\InstructorViewController::class, 'consultar_comite'])->name('instructorViews.consultar_comite');
     Route::get('/instructorViews/detalles_comite', [\App\Http\Controllers\InstructorViewController::class, 'detalles_comite'])->name('instructorViews.detalles_comite');
-    
+
     Route::get('/gestorComiteViews/detalles', [\App\Http\Controllers\GestorComiteViewsController::class, 'detalles'])->name('gestorComiteViews.detalles');
     Route::get('/gestorComiteViews/gFechas/{solicitud}', 'App\Http\Controllers\GestorComiteViewsController@gFechas')->name('gestorComiteViews.gFechas');
     Route::get('/gestorComiteViews/detalles/{solicitud}', 'App\Http\Controllers\GestorComiteViewsController@detalles')->name('gestorComiteViews.detalles');
     Route::delete('/gestor-comite-views/{solicitud}', 'App\Http\Controllers\GestorComiteViewsController@destroy')->name('gestorComiteViews.destroy');
-    
-//Estas son las rutas de la vista del aprendiz
+
+    //Estas son las rutas de la vista del aprendiz
     Route::get('/aprendiz/consultas', [\App\Http\Controllers\AprenController::class, 'consultas'])->name('aprendiz_Views.consultas');
     Route::post('/aprendiz/consultas', [\App\Http\Controllers\AprenController::class, 'consultas'])->name('aprendiz_Views.consultas');
-   
+
 
     Route::get('/aprendiz/plan_mejoramiento', [\App\Http\Controllers\AprenController::class, 'plan_mejoramiento'])->name('aprendiz_Views.plan_mejoramiento');
     Route::post('/aprendiz/plan_mejoramiento', [\App\Http\Controllers\AprenController::class, 'plan_mejoramiento']); // Sin un nombre de ruta específico para POST
@@ -110,7 +118,7 @@ Route::middleware([
     Route::get('/aprendiz/detalles', [\App\Http\Controllers\AprenController::class, 'detalles'])->name('aprendiz_Views.detalles');
 
     Route::get('/aprendiz/impugnaciones', [\App\Http\Controllers\AprenController::class, 'impugnaciones'])->name('aprendiz_Views.impugnaciones');
-    
+
 
     Route::post('/aprendiz/impugnaciones', [\App\Http\Controllers\AprenController::class, 'impugnaciones'])->name('aprendiz_Views.impugnaciones');
 
