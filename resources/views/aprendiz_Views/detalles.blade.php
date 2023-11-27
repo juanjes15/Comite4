cd5a6492b656065696464bd141fef3ea6680d2fc
Skeepx <maferestrepo90@gmail.com>
2023-11-27 09:38:14 -0500

    todo

--- resources/views/aprendiz_Views/detalles.blade.php
+++ resources/views/aprendiz_Views/detalles.blade.php
@@ -1,48 +1,31 @@
 <x-app-layout>
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Detalles de citación a comité ') }}
+        </h2>
+    </x-slot>
     <div class="bg-green-50 shadow-x">
         <div class="max-w-3x1 mx-auto p-4 mt-10">
             <div class="max-w-3xl mx-auto p-6 bg-green-50 shadow-xl rounded-lg">
                 <div class="flex items-center justify-between mb-4">
                 </div>
 
-                <h1 class="text-2xl font-semibold text-gray-800 mb-4">Detalles de citación a comité</h1>
+               
 
-                <div class="flex flex-wrap -mx-4 mb-4">
-                    <div class="w-1/2 px-4 mb-4">
-                        <label class="block text-sm font-medium text-gray-700">Código</label>
-                        <input type="text" class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-400"
-                            readonly value="{{ $solicitud->id }}">
-                    </div>
-
-                    <div class="w-1/2 px-4 mb-4">
-                        <label class="block text-sm font-medium text-gray-700">Fecha</label>
-                        <input type="date" class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-400"
-                            readonly value="{{ $solicitud->sol_fecha }}">
-                    </div>
+                <!-- Información del Solicitante -->
+                <div class="bg-green-100 p-4 rounded-lg mb-4">
+                    <h3 class="text-lg font-semibold mb-2">Información del Solicitante</h3>
+                    <ul class="list-disc pl-4">
+                        <li><strong>Código:</strong> {{ $solicitud->id }}</li>
+                        <li><strong>Fecha:</strong> {{ $solicitud->sol_fecha }}</li>
+                        <li><strong>Lugar:</strong> {{ $solicitud->sol_lugar }}</li>
+                        <li><strong>Instructor:</strong> {{ $instructor->ins_nombres }} {{ $instructor->ins_apellidos }}
+                        </li>
+                    </ul>
                 </div>
 
-                <div class="flex flex-wrap -mx-4 mb-4">
-                    <div class="w-1/2 px-4 mb-4">
-                        <label class="block text-sm font-medium text-gray-700">Lugar</label>
-                        <input type="text" class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-400"
-                            readonly value="{{ $solicitud->sol_lugar }}">
-                    </div>
-
-                    <div class="w-1/2 px-4 mb-4">
-                        <label class="block text-sm font-medium text-gray-700">Instructor</label>
-                        <input type="text" class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-400"
-                            readonly value="{{ $instructor->ins_nombres }} {{ $instructor->ins_apellidos }}">
-                    </div>
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-sm font-medium text-gray-700">Descripción de la Citación</label>
-                    <textarea class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-400" rows="4"
-                        readonly>{{ $solicitud->sol_asunto }}</textarea>
-                </div>
+                <x-link href="{{ route('aprendiz_Views.consultas') }}"
+                    class=" mx-5 mb-6 bg-green-700 hover:bg-red-800 border-2 border-green-950">Atras</x-link>
             </div>
-            <x-link href="{{ route('aprendiz_Views.consultas') }}"
-                class=" mx-5 mb-6 bg-green-700 hover:bg-red-800 border-2 border-green-950">Atras</x-link>
         </div>
-    </div>
 </x-app-layout>
